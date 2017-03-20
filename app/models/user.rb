7ff383574d1c880d9ea19e65cb1dde0f4b750d0f
wraj <waghrajesh6@gmail.com>
2017-03-20 16:28:41 +0530

    Added has-one-through association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,6 @@ class User < ApplicationRecord
 	has_and_belongs_to_many :events
 	has_many :enrollments
 	has_many :games, through: :enrollments
+	has_one :purse
+	has_one :payment_history, through: :purse
 end
