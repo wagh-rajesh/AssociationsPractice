4e3e5b55b491ec06ff8cd0a46798d0215e8b61c3
wraj <waghrajesh6@gmail.com>
2017-03-20 16:23:54 +0530

    Added one-to-one, one-to-many, many-to-many simple, has-many-through associations

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ApplicationRecord
+	has_many :posts, dependent: :destroy
+	has_one :address, dependent: :destroy
+	has_and_belongs_to_many :events
+	has_many :enrollments
+	has_many :games, through: :enrollments
+end
